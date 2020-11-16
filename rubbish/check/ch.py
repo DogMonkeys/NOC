e1e64afe97bf6b3a5bac75504bae7f2df5edaeae
pythonNLGS <pythonnlgs@qq.com>
2020-11-16 22:22:59 +0800

    g

--- rubbish/check/ch.py
+++ rubbish/check/ch.py
@@ -2,9 +2,12 @@ import random
 
 class Rand:
 	@classmethod
-	def chone(cls, dat):
+	def ch_names(cls):
 		names = ['易腐垃圾', '其他垃圾', '有害垃圾', '可回收物']
 		k = names[random.randint(0, 4)]
-		s = random.choice(dat[k])
-		return [k, s]
+		return k
 
+	@classmethod
+	def ch_datas_1(cls, type_, dat):
+		sl = dat.RUBBISHS[type_]
+		return random.chioce(sl)
